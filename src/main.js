97c07fba516ad69b46bb761272ce5054ba48d9f3
Diego Romeo <diego.romeo025@gmail.com>
2022-07-31 18:56:48 +0200

    new example simulation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {Engine} from "./core/engine.js";
 import {Pin} from "./constraints/pin.js";
 import {RigidBody} from "./bodies/rigid-body.js";
+import {DistanceConstraint} from "./constraints/distance-constraint.js";
 
 window.onload = function() {
   var engine = new Engine("simulation");
@@ -8,8 +9,10 @@ window.onload = function() {
   var w1 = new RigidBody({x:100, y:300}, {x:0, y:0}, {x:0, y:0}, 10);
   var pin = new Pin({x:640, y:0}, w1);
 
-  engine.world.add(w1);
-  engine.world.add(pin);
+  var w2 = new RigidBody({x:200, y:300}, {x:0,y:0}, {x:0, y:0}, 10);
+  var dist_constr = new DistanceConstraint(w1, w2);
+
+  engine.world.add(w1, w2, pin, dist_constr);
 
   engine.start();
 }
