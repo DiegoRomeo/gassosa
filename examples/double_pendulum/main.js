de63f6947c970fd62a901bcbb906ad2370ded0dd
Diego Romeo <diego.romeo025@gmail.com>
2022-08-03 14:51:30 +0200

    :hammer: Change tha mass of the second weight to test the constraint algorithm

--- examples/double_pendulum/main.js
+++ examples/double_pendulum/main.js
@@ -9,7 +9,7 @@ window.onload = function() {
   var w1 = new RigidBody({x:100, y:300}, {x:0, y:0}, {x:0, y:0}, 10);
   var pin = new Pin({x:640, y:0}, w1);
 
-  var w2 = new RigidBody({x:200, y:300}, {x:0,y:0}, {x:0, y:0}, 10);
+  var w2 = new RigidBody({x:200, y:300}, {x:0,y:0}, {x:0, y:0}, 30);
   var dist_constr = new DistanceConstraint(w1, w2);
 
   engine.world.add(w1, w2, pin, dist_constr);
